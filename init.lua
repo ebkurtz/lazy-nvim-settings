338c076a7ff23900e00b282d3fa26b3db9b9677b
Elliott <ekurtz@chesapeakeconservancy.org>
2023-12-01 10:05:44 -0500

    Stop autoformat on save

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.o.background = "light"
+return {
+  lsp = {
+    formatting = {
+      format_on_save = false,
+    },
+  },
+}
