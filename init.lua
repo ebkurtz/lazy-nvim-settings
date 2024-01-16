f99d30c9cdd825520878fdcf582b385f950427dc
Elliott <ekurtz@chesapeakeconservancy.org>
2024-01-16 14:49:36 -0500

    Disable plugin update notifications

--- init.lua
+++ init.lua
@@ -1,10 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.o.background = "light"
-return {
-  lsp = {
-    formatting = {
-      format_on_save = false,
-    },
-  },
-}
